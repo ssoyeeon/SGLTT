427b236907ab1a7170e34b4b96f0906206687cbe
ssoyeon <ssuu2039@gmail.com>
2024-07-27 14:09:31 +0900

    [feat] 8번출구

--- Amu/Assets/Scripts/ChackSiGame.cs
+++ Amu/Assets/Scripts/ChackSiGame.cs
@@ -11,40 +11,35 @@ public class ChackSiGame : MonoBehaviour
 
     public Transform other;                                     //일단 아이 하나를 잡아왔어요
     public float speed = 20f;
-    private Rigidbody characterRigidbody;
+
+    public Rigidbody rig;
 
     public Camera camera;
-    public float distance = 10f;
+    public float distance = 10f;                                //레이 거리에여 
 
 
     void Start()
     {
-        characterRigidbody = GetComponent<Rigidbody>();         //리지드바디 내놓으세요!!
+       
     }
 
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");             //움직임 설정
-        float inputZ = Input.GetAxis("Vertical");               //움직임 설정
-
-        Vector3 velocity = new Vector3(inputX, 0, inputZ);      //움직임 설정
-        velocity *= speed;
-        characterRigidbody.velocity = velocity;
-
         if (Input.GetMouseButton(0))
         {
+            rig.angularVelocity = Vector3.zero;
 
-            RaycastHit hit;
-            Vector3 mouseScreenPosition = Input.mousePosition;
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;                                         //레이를 쏴요
+            Vector3 mouseScreenPosition = Input.mousePosition;      //마우스 포지션을 Vector3 변수에 넣어요
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition); //레이를 파우스 포지션에 쏴요
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit))                      //만약 레이가 맞았다면
             {
-                if (hit.collider.tag == "Cube")
+                if (hit.collider.tag == "Cube")                     //만약 레이를 맞은게 큐브(아이)라면
                 {
-                    GameObject objectHit = hit.collider.gameObject;
-                    mouseScreenPosition.z = distance;
-                    objectHit.transform.position = camera.ScreenToWorldPoint(mouseScreenPosition);
+                    GameObject objectHit = hit.collider.gameObject; //레이 쏴요!
+                    mouseScreenPosition.z = distance;               //z는 거리에여 10이에요!
+                    objectHit.transform.position = camera.ScreenToWorldPoint(mouseScreenPosition);  //오브젝트를 마우스 포지션에 따라 움직여요
                 }
             }
 
@@ -54,10 +49,10 @@ public class ChackSiGame : MonoBehaviour
                 //Debug.Log("Distance to other: " + dist);
                 other.localScale = new Vector3(dist * 0.4f, dist * 0.4f, dist * 0.4f);      //거리에 따라 스케일 값을 변경해요
 
-                if (Input.GetKey(KeyCode.E))
+                if (Input.GetKey(KeyCode.E))        //E를 누르면요
                 {
-                    other.transform.Rotate(0f, -Input.GetAxis("Mouse X") * speed, 0f, Space.World);
-                    other.transform.Rotate(-Input.GetAxis("Mouse Y") * speed, 0f, 0f);
+                    other.transform.Rotate(0f, -Input.GetAxis("Mouse X") * speed, 0f, Space.World); //돌아간당
+                    other.transform.Rotate(-Input.GetAxis("Mouse Y") * speed, 0f, 0f);              //빙글빙글
                 }
             }
         }
---
+++ Amu/Assets/Scripts/eight.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class eight : MonoBehaviour
+{
+    //제대로 갔다면 다음 무작위 씬으로 넘어감. 번호 +1
+    //틀렸다면 다음 무작위 씬으로 넘어가고, 번호가 초기화 됨 
+    //만약 이상현상이 씬 4, 6, 7, 9 에 있으면 걔네 어떻게 할거야? 
+    //4,6,7,9에만 게임 오브젝트를 두고 그 게임 오브젝트가 있다면 없다면으로 체크해서 하는건?
+
+    public bool isTrue;     //제대로 갔나염?
+    public int scene = Random.Range(0, 10);
+    public int exit = 0;
+
+    void Update()
+    {
+        if(isTrue == true)                      //맞췄을 때
+        {
+            exit++;                             //출구 번호 +1
+        }
+
+        else if ( isTrue == false)              //틀렸을 때
+        {
+            exit = 0;                           //출구 번호 0번. (초기화)
+        }
+
+        if(isTrue)                              //일단 어디든 갔을 때
+        {
+            SceneManager.LoadScene(scene);      //랜덤 0~10 씬 출력
+        }
+
+        Scene nowscene = SceneManager.GetActiveScene();     //현재 씬을 가져오고
+        if (nowscene.buildIndex == 4)                       //현재 씬이 4번이면 ( 이상한 씬 )
+        {
+            //앞 뒤로 트리거 설정할 게임 오브젝트 한 개씩 두고 그 게임 오브젝트를 지나갔는지 판별하면 될 것 같네요.
+        }
+    }
+}
