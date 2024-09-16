39810b789aa8221e92f75ea8d18438d4cad8a00a
ssoyeeon <ssuu2039@gmail.com>
2024-09-16 13:40:09 +0900

    [feat] 문에 닿으면 다음 씬으로 가보아요

---
+++ Amu/Assets/Scripts/ChackSiGame/Door.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+            Scene scene = SceneManager.GetActiveScene();
+            int curScene = scene.buildIndex;
+            int nextScene = curScene + 1;
+            SceneManager.LoadScene(nextScene);
+    }
+}
