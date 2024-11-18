0b87e62d8d60c6044c3c699a05d2ec9150db0cd6
ssoyeeon <ssuu2039@gmail.com>
2024-11-18 12:56:43 +0900

    [feat] Portal

---
+++ Amu/Assets/Scripts/TeamProject/DictionaryTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictionaryTest : MonoBehaviour
+{
+    Dictionary<int, string> test = new Dictionary<int, string>();
+
+    void Start()
+    {
+        test.Add(1, "∫Ì∑∞ ¿Â≥≠∞®");
+        test.Add(2, "∑Œ∫ø");
+        test.Add(3, "ª°∞£ √•");
+    }
+
+    void Update()
+    {
+        
+    }
+}
