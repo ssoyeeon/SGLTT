abe5ca9b8e508edbbaedac06758b0cd605b31d12
ssoyeeon <ssuu2039@gmail.com>
2024-12-10 19:18:41 +0900

    [feat] Property 도 배웠어여!!

--- Amu/Assets/Scripts/Potal/Test.cs
+++ Amu/Assets/Scripts/Potal/Test.cs
@@ -2,17 +2,38 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Test : MonoBehaviour
+public class Test : amugerna
 {
     public Transform tr;
     public TT[] TTs;
-    public int index;
+    public int index = 1;
+    public int tempIndex = 0;
+    public SpriteRenderer spriteRenderer;
+    public Sprite IMG
+    {
+        get { return sr; }
+    }
+    public int property
+    {
+        get { return index; }
+        set 
+        { 
+            if(tempIndex+value > 10)
+            {
+                return;
+            }
+            tempIndex =value;
+        }
+    }
+
     void Start()
     {
         TTs = new TT[2];
         TTs[0] = new OO();
         TTs[1] = new PPAP();
+        spriteRenderer.sprite = IMG;
     }
 
     // Update is called once per frame
@@ -20,7 +41,8 @@ public class Test : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            TTs[index].aa();
+            Debug.Log(property);
+            property += 2;
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
@@ -31,6 +53,10 @@ public class Test : MonoBehaviour
             index++;
         }
     }
+}
+public class amugerna : MonoBehaviour
+{
+    protected Sprite sr;
 }
 public interface TT
 {
