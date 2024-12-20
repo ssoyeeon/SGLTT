521d8ced11819785988479aa76c550a0620bb4ed
ssoyeeon <ssuu2039@gmail.com>
2024-12-20 14:42:42 +0900

    [feat] 주사위 돌리기 끝!

--- Amu/Assets/Scripts/GameMath/Dice.cs
+++ Amu/Assets/Scripts/GameMath/Dice.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Dice : MonoBehaviour
 {
     public Rigidbody ri;
+    public TMP_Text intText;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
+        if(Input.GetKeyDown(KeyCode.R))
         {
-            this.gameObject.transform.position = new Vector3(-3, 2, -7);
+            int fint = Random.Range(1, 4000);
+            int dint = Random.Range(1, 4000);
+            int rint = Random.Range(1, 4000);
+
+            ri.AddForce(Vector3.up * 200);
+            ri.AddTorque(new Vector3(fint, dint, rint), ForceMode.VelocityChange);
         }
         if(Input.GetMouseButtonDown(0))
         {
@@ -21,26 +28,32 @@ public class Dice : MonoBehaviour
 
             if (udot >= 0.9f)
             {
+                intText.text = "5";
                 Debug.Log("5");
             }
             else if (udot <= -0.9f)
             {
+                intText.text = "2";
                 Debug.Log("2");
             }
             else if (fdot >= 0.9f)
             {
+                intText.text = "1";
                 Debug.Log("1");
             }
             else if (fdot <= -0.9f)
             {
+                intText.text = "6";
                 Debug.Log("6");
             }
             else if (rdot >= 0.9f)
             {
+                intText.text = "3";
                 Debug.Log("3");
             }
             else if (rdot <= -0.9f)
             {
+                intText.text = "4";
                 Debug.Log("4");
             }
         }
